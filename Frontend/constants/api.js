d6dfb7d063328eecd8042eb12fd9afe469f374de
Kalana WIJEKOON M GAMAGED <KALANAW@our.ecu.edu.au>
2025-01-24 13:20:55 +0530

    Revert "Add Frontend Source code"
    This reverts commit 9e98be5e4105d67ab268f6c77b309721640b2f9f.

--- Frontend/constants/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiClient = axios.create({
-  baseURL: 'https://9969-61-245-161-118.ngrok-free.app', // Replace with your backend URL
-  timeout: 10000, // Timeout in milliseconds
-});
-
-export default apiClient;
